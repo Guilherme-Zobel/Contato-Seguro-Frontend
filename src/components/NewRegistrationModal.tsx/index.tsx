7df0b5f72bfa74c7c4e99c5900b421dfa6750b0e
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-01 20:29:53 -0300

    feat: Adiciona estrutura de inputs e botões ao modal

--- src/components/NewRegistrationModal.tsx/index.tsx
+++ src/components/NewRegistrationModal.tsx/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal'
+import closeImg from '../../assets/close.svg';
+import { Cointainer, StyledSideBySideInputs } from './styles';
 
 interface NewRegistrationModalProps {
   isOpen: boolean;
@@ -8,10 +10,58 @@ interface NewRegistrationModalProps {
 export function NewRegistrationModal({ isOpen, onRequestClose}: NewRegistrationModalProps) {
   return (
     <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}                  
-      >
-        <h2>Inserir</h2>
-    </Modal>
+          isOpen={isOpen}
+          onRequestClose={onRequestClose}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"                
+        >
+          <button 
+            type="button" 
+            onClick={onRequestClose} 
+            className="react-modal-close"
+          >
+            <img src={closeImg} alt="Fechar modal" />
+          </button>
+          <Cointainer>
+            <h2>Inserir</h2>
+
+            <input
+              placeholder='Nome'
+            />
+            <input
+              type="email"
+              placeholder='E-mail'
+            />
+             <StyledSideBySideInputs>
+            <input
+              type="number"
+              placeholder='Telefone'
+            />
+
+            <input
+              type="date"
+              placeholder='Data de Nascimento'
+            />
+             </StyledSideBySideInputs>
+
+             <input
+              placeholder='Cidade onde nasceu'
+            />
+            
+            <StyledSideBySideInputs>
+              <button
+                type="button"
+              >
+                <span>Limpar</span>
+              </button>
+
+              <button
+                type="submit"
+              >
+                <span>Enviar</span>
+              </button>
+            </StyledSideBySideInputs>
+          </Cointainer>
+        </Modal>
   );
 }
--- src/components/NewRegistrationModal.tsx/styles.ts
+++ src/components/NewRegistrationModal.tsx/styles.ts
@@ -2,3 +2,6 @@ import styled from "styled-components";
 
 export const Cointainer = styled.div`
 `;
+
+export const StyledSideBySideInputs = styled.div`
+`;
