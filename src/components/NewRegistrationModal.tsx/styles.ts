f8fac24df9397a25f3841563c9a4287645633179
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-01 22:39:57 -0300

    style: Concluída estilização do componente NewRegistrationModal

--- src/components/NewRegistrationModal.tsx/styles.ts
+++ src/components/NewRegistrationModal.tsx/styles.ts
@@ -1,7 +1,82 @@
 import styled from "styled-components";
+import { darken } from 'polished'
 
-export const Cointainer = styled.div`
+export const Cointainer = styled.form`
+ h2 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    color: var(--gray-900);
+    border-bottom: 1px solid var(--gray-300);
+  }
+  input {
+    width: 100%;
+    transition: border-color 0.2s;
+    padding: 0 1.5rem;
+    margin-top: 1rem;
+    height: 4rem;
+    font-weight: 400;
+    font-size: 1rem;
+    border: 1px solid var(--gray-200);
+    border-radius: 0.25rem;
+    background: var(--gray-150);
+
+    &::placeholder {
+      color: var(--grey--900);
+    }
+
+    &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0;
+    border-color: ${darken(0.1, '#d7d7d7')};
+    } 
+  }
+
+  select {
+    width: 100%;
+    transition: border-color 0.2s;
+    padding: 0 1.5rem;
+    height: 4rem;
+    font-weight: 400;
+    font-size: 1rem;
+    border: 1px solid var(--gray-200);
+    border-radius: 0.25rem;
+    background: var(--gray-150);
+
+    &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0;
+    border-color: ${darken(0.1, '#d7d7d7')};
+    } 
+  }
 `;
 
 export const StyledSideBySideInputs = styled.div`
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+  display: grid;
+  button {
+    transition: border-color 0.2s;
+    margin-top: 2rem;
+    justify-content: center;
+    height: 4rem;
+    display: flex;
+    color: var(--white);
+    border: 1px solid var(--gray-200);
+    border-radius: 0.25rem;
+    background: var(--gray-850);
+    align-items: center;
+    &:hover {
+      border-color: ${darken(0.1, '#323238')};
+    }
+    img {
+      width: 20px;
+      height: 20px;
+    }
+    span {
+      margin-left: 1rem;
+      font-size: 1rem;
+      display: inline-block;
+      color: var(--text-title);
+    }
+  }
 `;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,11 @@ import backgroundImage from '../assets/backgound.jpg'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
+  --white: #fff;
+
   --gray-100: #e1e1e6;
+  --gray-150: #e7e9ee;
+  --gray-200: #d7d7d7;
   --gray-300: #a8a8b3;
   --gray-700: #323238;
   --gray-800: #29292e;
@@ -34,7 +38,6 @@ export const GlobalStyle = createGlobalStyle`
     background-repeat: no-repeat;
     background-position: center center;
     -webkit-font-smoothing: antialiased;
-
   }
 
   body, input, textarea, button {
@@ -49,4 +52,37 @@ export const GlobalStyle = createGlobalStyle`
   button {
     cursor: pointer;
   }
-  `
+
+.react-modal-overlay {
+    top: 0;
+    right: 0;
+    position: fixed;
+    left: 0;
+    justify-content: center;
+    display: flex;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    align-items: center;
+  }
+
+   .react-modal-content {
+     width: 100%;
+     position: relative;
+     padding: 3rem;
+     max-width: 576px;
+     border-radius: 0.25rem;
+     background: var(--gray-100);
+   }
+   
+  .react-modal-close {
+    transition: filter 0.2s;
+    top: 1.5rem;
+    right: 1.5rem;
+    position: absolute;
+    border: 0;
+    background: transparent;
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+`
