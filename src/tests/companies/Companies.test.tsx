6888779c1292c0ef7c6d1c913e305d9dd0039d84
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-04 04:24:29 -0300

    test: Deve fechar o CompanyModal

--- src/tests/companies/Companies.test.tsx
+++ src/tests/companies/Companies.test.tsx
@@ -13,3 +13,16 @@ test("Should open an insert modal", () => {
   expect(screen.getByText("Inserir"));
 });
 
+test("Should close the insert modal", () => {
+  const mockCloseModal = jest.fn();
+  render(
+    <CompanyContext.Provider value={mockCompanyContext}>
+      <CompanyModal isOpenModal={true} closeModal={mockCloseModal} />
+    </CompanyContext.Provider>
+  );
+
+  const closeButton = screen.getByAltText("Fechar modal");
+
+  fireEvent.click(closeButton);
+  expect(mockCloseModal).toBeCalled();
+});
