65639673ccba4875fd9084499b157a3ff648c2a5
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-03 21:51:41 -0300

    feat: CompanyContext

---
+++ src/Context/CompanyContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, ReactNode, useState, useEffect } from "react";
+import companyData from "../data/company.json";
+
+export interface ICompanyValue {
+  id: number;
+  name: string;
+  phone: string;
+  cnpj: string;
+  address: string;
+}
+
+type ContextProps = {
+  children: ReactNode;
+};
+
+export type ContextData = {
+  searchValue: string;
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+  columnFilter: keyof ICompanyValue;
+  setColumnFilter: React.Dispatch<React.SetStateAction<keyof ICompanyValue>>;
+  companyValue: ICompanyValue[];
+  setCompanyValue: React.Dispatch<React.SetStateAction<ICompanyValue[]>>;
+  idRegistration: number;
+  setIdRegistration: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const CompanyContext = createContext<ContextData>({} as ContextData);
+
+export const CompanyProvider = (props: ContextProps) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [columnFilter, setColumnFilter] = useState(
+    "name" as keyof ICompanyValue
+  );
+  const [companyValue, setCompanyValue] = useState(companyData.rows);
+  const [idRegistration, setIdRegistration] = useState(0);
+
+  const providerValue: ContextData = {
+    searchValue,
+    setSearchValue,
+    columnFilter,
+    setColumnFilter,
+    companyValue,
+    setCompanyValue,
+    idRegistration,
+    setIdRegistration,
+  };
+
+  return <CompanyContext.Provider value={providerValue} {...props} />;
+};
