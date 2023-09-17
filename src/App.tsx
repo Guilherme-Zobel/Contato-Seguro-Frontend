f24f2ff497cb2a5324aafd8b7654bfcb5675e596
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-17 16:47:18 -0300

    refactor: componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,18 @@
 import Modal from "react-modal";
-import { useState } from "react";
-import { UserSection } from "./components/Section/UserSection";
 import { GlobalStyle } from "./styles/global";
-import { UserModal } from "./components/Modal.tsx/UserModal";
 import { UserProvider } from "./Context/UserContext";
 import { Container } from "./components/Section/styles";
 import { Tabs } from "./components/Tabs";
-import { CompanySection } from "./components/Section/CompanySection";
 import { CompanyProvider } from "./Context/CompanyContext";
-import { CompanyModal } from "./components/Modal.tsx/CompanyModal";
-import dictionary from "./utils/dictionary";
 
 if (process.env.NODE_ENV !== "test") Modal.setAppElement("#root");
 
 export function App() {
-  const [isOpenModal, setIsOpenModal] =
-    useState(false);
-  const [selectedSection, setSelectedSection] = useState(
-    dictionary.userSection
-  );
-
-  function handleOpenModal() {
-    setIsOpenModal(true);
-  }
-
-  function handleCloseModal() {
-    setIsOpenModal(false);
-  }
-
   return (
     <CompanyProvider>
       <UserProvider>
         <Container>
-          <Tabs
-            setSelectedSection={setSelectedSection}
-            selectedSection={selectedSection}
-          />
-          {selectedSection === dictionary.userSection && (
-            <>
-              <UserSection handleOpenModal={handleOpenModal} />
-              <UserModal
-                isOpenModal={isOpenModal}
-                closeModal={handleCloseModal}
-              />
-            </>
-          )}
-          {selectedSection === dictionary.companySection && (
-            <>
-              <CompanySection handleOpenModal={handleOpenModal} />
-              <CompanyModal
-                isOpenModal={isOpenModal}
-                closeModal={handleCloseModal}
-              />
-            </>
-          )}
+          <Tabs />
         </Container>
         <GlobalStyle />
       </UserProvider>
