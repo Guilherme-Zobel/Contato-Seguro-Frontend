e806177418166f2837adad866e7cf91bb2c33d57
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-09-17 16:50:24 -0300

    refactor: Tabs

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,27 +1,40 @@
-import { Container } from "./styles";
+import { useState } from "react";
+import { Container, selectedTab } from "./styles";
+import { UserSection } from "../Section/UserSection";
+import { CompanySection } from "../Section/CompanySection";
+import { UserModal } from "../Modal.tsx/UserModal";
+import { CompanyModal } from "../Modal.tsx/CompanyModal";
 import dictionary from "../../utils/dictionary";
 
-interface TabsProps {
-  setSelectedSection: React.Dispatch<React.SetStateAction<number>>;
-  selectedSection: number;
-}
-
-export function Tabs({setSelectedSection, selectedSection}: TabsProps) {
+export function Tabs() {
+  const [selectedSection, setSelectedSection] = useState(dictionary.userSection);
 
   return (
     <>
     <Container>
       <button
-        style={selectedSection === dictionary.userSection ? {fontWeight: "600", borderColor: "#a8a8b3"} : {}}
+        style={selectedSection === dictionary.userSection ? selectedTab : {}}
         onClick={() => setSelectedSection(dictionary.userSection)}>
         Usuários
       </button>
       <button
-        style={selectedSection === dictionary.companySection ? {fontWeight: "600", borderColor: "#a8a8b3"} : {}}
+        style={selectedSection === dictionary.companySection ? selectedTab : {}}
         onClick={() => setSelectedSection(dictionary.companySection)}>
         Empresas
       </button>
     </Container>
+    {selectedSection === dictionary.userSection && (
+        <>
+          <UserSection />
+          <UserModal />
+        </>
+    )}
+    {selectedSection === dictionary.companySection && (
+        <>
+          <CompanySection/>
+          <CompanyModal/>
+        </>
+    )}
     </>
   )
 }
